61db789fdaefb0b9b2469c572104d8a50db7eec8
tung nguyen <tungnbhla1@gmail.com>
2021-04-01 10:23:34 +0700

    up

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -13,3 +13,20 @@ module.exports = {
   },
   plugins: [],
 }
+// tailwind.config.js
+module.exports = {
+  theme: {
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+      'cssz25': '25px',
+      'cssz40': '40px',
+    }
+  }
+}
